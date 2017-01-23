afef444f16216db6b86247bcc5ddc6615788da8e
Nick Villano <nvillano80@malvernprep.org>
2017-01-22 19:38:06 -0500

    Created the .sh that tells the API to donate $1

---
+++ RaspberryPiAlarm/punish.sh
@@ -0,0 +1,2 @@
+curl -X POST --header "Content-Type: application/json" --header "Accept: application/json" -d "{\"merchant_id\": \"57cf75cea73e494d8675ec49\",\"medium\": \"balance\",\"purchase_date\": \"2017-01-22\",\"amount\": 1,\"description\": \"string\"}" "http://api.reimaginebanking.com/accounts/5883e3351756fc834d8ebe89/purchases?key=b84d3a153e2842b8465bcc4fde3d1839"
+echo done!
